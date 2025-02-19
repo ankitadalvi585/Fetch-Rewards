bbfc7dd4f9f6573cd1a234b92a6e1cee55020a3a
Ankita Ashok Dalvi <115281341+ankitadalvi585@users.noreply.github.com>
2025-02-19 03:14:41 -0500

    Add files via upload

---
+++ 2. FetchRewardsAssessment.sql
@@ -0,0 +1,89 @@
+-- Second: Write queries that directly answer predetermined questions from a business stakeholder
+
+-- Question 1:
+-- When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+
+-- Considered 'FINISHED' as 'ACCEPTED' as it felt relevant as compared to other categories in rewardsReceiptStatus column
+-- Calculated average of totalSpent grouped by rewardsReceiptStatus column to find the average amount spent for each status
+
+SELECT rewardsReceiptStatus, ROUND(AVG(totalSpent),2) AS averageSpend 
+FROM Receipts
+WHERE rewardsReceiptStatus in ('FINISHED', 'REJECTED')
+GROUP BY rewardsReceiptStatus
+ORDER BY AVG(totalSpent) DESC;
+
+
+
+-- Question 2:
+-- When considering total number of items purchased from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+
+-- Considered 'FINISHED' as 'ACCEPTED' as it felt relevant as compared to other categories in rewardsReceiptStatus column
+-- Calculated sum of purchasedItemCount grouped by rewardsReceiptStatus column to find out no of purchased items for each status
+
+SELECT rewardsReceiptStatus, SUM(purchasedItemCount) as totalItems 
+FROM Receipts
+WHERE rewardsReceiptStatus in ('FINISHED', 'REJECTED')
+GROUP BY rewardsReceiptStatus
+ORDER BY sum(purchasedItemCount) DESC;
+
+
+
+-- Question 3:
+-- Which brand has the most spend among users who were created within the past 6 months? 
+
+-- Joined Receipts with RewardReceiptItemList tables using id column as the key
+-- Joined Users and Receipt tables using 'id' & 'userId' respectively as the keys
+-- Used DATEADD function to filter the users created within the past 6 months from the MAX (most recent) createdDate in the users table
+-- Used sum of totalSpent column and grouped by brandCode to calculate totl amount spent for each brand
+-- brandCode column has multiple NULL values, hence only considered the rows in 'RewardReceiptItemList' where brandCode is not NULL
+
+WITH ReceiptTotals AS (
+    SELECT 
+        ri.brandCode,
+        ROUND(SUM(r.totalSpent),2) AS summedTotalSpent
+    FROM 
+		receipts r INNER JOIN RewardReceiptItemList ri ON r.id=ri.id
+		INNER JOIN users u ON r.userId = u.id
+    WHERE u.createdDate > DATEADD(MONTH, -6, (Select MAX(createddate) from users)) 
+          AND ri.brandCode IS NOT NULL
+    GROUP BY ri.brandCode
+)
+SELECT top 1 *
+FROM ReceiptTotals
+ORDER BY summedTotalSpent DESC;
+
+
+
+-- Question 4:
+-- Which brand has the most transactions among users who were created within the past 6 months?
+
+-- Joined Receipts with RewardReceiptItemList tables using id column as the key
+-- Joined Users and Receipt tables using 'id' & 'userId' respectively as the keys
+-- Used DATEADD function to filter the users created within the past 6 months from the MAX (most recent) createdDate in the users table
+-- Used count of 'id' from Receipts table  and grouped by brandCode to calculate number of transactions for each brand
+-- brandCode column has multiple NULL values, hence only considered the rows in 'RewardReceiptItemList' where brandCode is not NULL
+
+WITH ReceiptCount AS (
+    SELECT 
+        ri.brandCode,
+        COUNT(r.id) AS transactionCount
+    FROM 
+		receipts r INNER JOIN RewardReceiptItemList ri ON r.id=ri.id
+		INNER JOIN users u ON r.userId = u.id
+    WHERE 
+		u.createdDate > DATEADD(MONTH, -6, (Select MAX(createddate) from users)) 
+		AND ri.brandCode IS NOT NULL
+    GROUP BY 
+		ri.brandCode
+)
+SELECT top 1 *
+FROM ReceiptCount
+ORDER BY transactionCount DESC;
+
+
+
+
+
+
+
+
---
+++ 3. FetchRewardsAssessment_DQ.sql
@@ -0,0 +1,51 @@
+-- Third: Evaluate Data Quality Issues in the Data Provided
+
+-- 1. JSON Files: 
+--    Date format in json files is in epoch time format (Unix Time stamp)
+
+-- 2. Found brandcodes in RewardReceiptItems table which are not present in brands table and vice versa. 
+--    Considering Brands as the dimension table, all of the brandcodes in RewardReceiptItems table should be present in Brands table.
+--	  Significant amount of data will be missing when we query between Brands & RewardReceiptItems tables using joins.
+
+Select distinct brandcode from RewardReceiptItemList order by brandcode;
+Select distinct brandcode from Brands order by brandcode;
+
+Select distinct b.brandcode, ri.brandcode 
+from RewardReceiptItemList ri inner join Brands b
+on ri.brandCode = b.brandcode;
+
+Select distinct b.brandcode, ri.brandcode 
+from RewardReceiptItemList ri left join Brands b
+on ri.brandCode = b.brandcode;
+
+Select distinct b.brandcode, ri.brandcode 
+from RewardReceiptItemList ri right join Brands b
+on ri.brandCode = b.brandcode;
+
+-- 3. Duplicate records in the Users table were removed before importing data from csv into SQL Server.
+
+-- 4. Data Consistency issues for categorical (text) columns.
+
+--	  For example: 
+--    a. name and brandCode columns in Brands table have somewhat similar data (differs in case). Also, brandcode contains more Null values as compared to name column. .
+--       createdDate & dateScanned and finishedDate & modifyDate in Receipts table have somewhat same data. Can we drop one?
+
+Select name, brandcode from Brands;
+Select createDate, dateScanned from Receipts;
+Select finishedDate, modifyDate from Receipts;
+
+--	  b. In category & categoryCode columns we can merge categories if possible like Dairy - Dairy & Refrigerated, Beauty - Beauty & Personal Care. 
+--	     We can make the data consistent in both columns by changing the case and can fill NULL values in categoryCode by referencing data in category column in Brands table.
+
+Select distinct category, categoryCode from Brands;
+Select distinct name, brandCode from Brands;
+
+--    c. The ReceiptItemsList table contains approximately 20 columns, with over 80% of the data in these columns being NULL. Can we drop them?
+--    d. Some boolean columns have NULL values.
+--       > needsFetchReview,userFlaggedNewItem,competitiveProduct in RewardReceiptItems 
+--       > topBrand from Brands
+
+Select needsFetchReview,userFlaggedNewItem,competitiveProduct from RewardReceiptItemList;
+Select topbrand from Brands;
+
+--    e. Some Barcodes(82) in RewardReceiptItems table are in text format.
